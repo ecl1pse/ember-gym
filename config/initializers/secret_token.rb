2e45d0b207780b9298bb6add2da18f2f65581164
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-11-14 17:48:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberGym::Application.config.secret_key_base = 'efff7afdae788019328d3299cb867338525b64c88649b171b2d4f59e741d8d079c9b162df1aba8f2fa43805a1213bc01716f28a199746a3426f94acb37ac5765'
